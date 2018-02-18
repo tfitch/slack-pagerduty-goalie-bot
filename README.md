0608c3c8ef79e1d7e115f42441c93e93a0f55a12
Tyler Fitch <fitch@adobe.com>
2018-02-17 18:20:48 -0800

    First pieces of functionality. Changes topic of Slack room.
    More to come, but this is a functional example for me. This will
    set the topic of a Slack room to have the name of the On Call
    Engineer from PagerDuty.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # slack-pagerduty-goalie-bot
-A Slack bot written in Node.js that will keep track of who is currently on-call in PagerDuty.
+
+This bot will set the Topic in a Slack channel with the name of the on-call engineer for your PagerDuty escalation policy.
+
+## Setting the necessary Environment Variables
+Check out the `.env.EXAMPLE` file for the values you need to set as Environment Variables in the process that will execute this job.
+
+This will vary depending on the method you use to run this script.
+
+The `SLACK_TOKEN` value here is what Slack calls a Legacy Token and for me runs as a user, not a bot. This is because the APIs I'm trying to use - setting Channel topic and managing a User Group called "Goalie" are API calls a bot *cannot* make but a user can.  Slack is working to address this gap with Workspace Tokens, but Workspaces Tokens do not (as of Feb 2018) work with Enterprise Grid accounts and that is what I use at work.
+
+## TODO: Automating the running on this bot
+* Manage a Slack user group alias called "Goalie" so users on your team can ask for help by asking `@goalie` a question.
+* I am expecting to run this as a Lambda function, but it could also just be a Jenkins job. It could change significantly depending on the requirements to run in those environments.  So far this just works in my terminal.
+* There is no persistence to know if the On Call Engineer has changed. So running this every five minutes will basically spam your Slack channel.
+* Schedule your running of the process based on when the average shift change occurs for your team.
+* Make this an actual "bot" that could respond to the question "Who is on call?"
---
+++ index.js
@@ -0,0 +1,63 @@
+// load our Configuration
+const env = require('node-env-file');
+env(__dirname + '/.env');
+
+// Import modules
+const pdClient = require('node-pagerduty');
+const jsonQuery = require('json-query');
+const Slack = require('slack')
+
+// PagerDuty vars
+const pd_time_zone = process.env.PAGERDUTY_TIME_ZONE
+const pd_escalation_policy_id = process.env.PAGERDUTY_ESCALATION_POLICY_ID
+const pd_api_key = process.env.PAGERDUTY_READONLY_TOKEN;
+const pd = new pdClient(pd_api_key);
+
+// Slack vars
+const slack_token = process.env.SLACK_TOKEN
+const slack_channel_id = process.env.SLACK_CHANNEL_ID
+const slack_channel_name = process.env.SLACK_CHANNEL_NAME
+const bot = new Slack({slack_token})
+// Test your Slack token by uncommenting this API
+// bot.api.test({korben:'dallas'}).then(console.log)
+
+bot.channels.join({token:slack_token, name:slack_channel_name}).then(console.log);
+// bot.chat.postMessage({token:slack_token, channel:slack_channel_name, text:"slack-pagerduty-goalie-bot testing API"}).then(console.log);
+
+// lookup parameters for PagerDuty
+let myPDlookupParams = {
+  time_zone: pd_time_zone,
+  'escalation_policy_ids[]': pd_escalation_policy_id
+};
+
+let OnCalls = function(yourQs) {
+  return pd.onCalls.listAllOnCalls(yourQs)
+}
+
+let SREonCall = function(SREid) {
+  return pd.users.getUser(SREid);
+}
+
+let myOnCalls = OnCalls(myPDlookupParams);
+
+myOnCalls.then(function(result) {
+  // The user with the value of "escalation_level=1" is the first responder but not obj[0] in the reply.
+  // numbers higher than 1 are probably management escalations. Uncomment the next line to see your values
+  //  console.log(result.body)
+  var SRE = jsonQuery('oncalls[escalation_level=1].user.id', {data: JSON.parse(result.body)})
+
+  // now that we know the SRE on call, get their email
+  let mySRE = SREonCall(SRE.value);
+
+  mySRE.then(function(result) {
+    // PageryDuty knows our email, Slack uses the username part
+    // so parse out the On Call Eng's email ID from their email
+    var sreOncallLDAPId = JSON.parse(result.body).user.email.split("@")[0];
+    // if needed, uncomment below to verify the ID you've found
+    // console.log(sreOncallLDAPId);
+
+    // now that we know the on call Engineer's email, use it in Slack
+    bot.channels.setTopic({token:slack_token, channel:slack_channel_id, topic:sreOncallLDAPId + " is the SRE on call."}).then(console.log);
+  })
+  
+})
